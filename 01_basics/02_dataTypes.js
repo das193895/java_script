fa81dcecdf2d5cba15fddb35d7bb70faa1653363
das193895 <112277910+das193895@users.noreply.github.com>
2023-08-28 14:08:46 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,27 @@
+let a = 10 ; // "number"
+
+let c = 10.567; // "number"
+
+let name = "Amlan"; // "string"
+
+let isLoggedIn = true; // "boolean"
+
+// null --> a stand alone value  --> specifies emptyness
+
+// undefined
+
+// symboole
+
+// bigInt
+
+
+/***********************************************************************/ 
+
+console.log(typeof 100.7); // number
+
+console.log(typeof "jjmfh"); // string
+
+console.log(typeof null);  // object 
+
+console.log(typeof undefined); // undefined
+
