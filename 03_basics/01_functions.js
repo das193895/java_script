55a2a5d72f57d923eaeb8ff6d3ba7a69407ff984
das193895 <112277910+das193895@users.noreply.github.com>
2023-12-18 19:34:14 +0000

    functions

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -22,3 +22,47 @@ function message_up(username){
         return `${username} just logged in`
     }
 }
+
+/****************************/
+
+function shoppingCart(...num){
+    return num;
+}
+
+console.log(shoppingCart(200,400,700,1000,900)); // [ 200, 400, 700, 1000, 900 ]
+
+function shoppingCartu(num1,num2,...num){
+    return num;
+}
+
+console.log(shoppingCartu(200,400,700,1000,900)); // [ 700, 1000, 900 ]
+
+/****************************/
+
+function obj(anyobject){   // anyobject refers to we are going to pass an object as an argumment
+
+    console.log(`username is ${anyobject.username} and price is ${anyobject.price}`)
+
+}
+
+obj({
+    username:"SB",
+    price:456
+})
+
+/****************************/
+
+
+console.log(addone(5)); // It will execute
+
+function addone(num){
+    return num+1;
+}
+
+
+// console.log(addtwo(5));  // it will not execute and will give an error
+const addtwo = function(num){
+    return num+2;
+}
+
+console.log(addtwo(5));
